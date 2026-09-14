7fc366829397aa4bab36833cf80491c87a2505df
IT25102017 <it25102017@my.sliit.lk>
2026-09-14 18:56:10 +0530

    Initialize Spring Boot project with SQL Server connection

---
+++ src/test/java/com/lankatex/smarttextile/SmartTextileManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lankatex.smarttextile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTextileManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
