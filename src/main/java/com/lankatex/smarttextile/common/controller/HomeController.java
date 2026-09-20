05064d7bab5f3b7de4bcdaf8de69bd5197a2c2aa
IT25102017 <it25102017@my.sliit.lk>
2026-09-20 17:33:18 +0530

    Add login state and logout functionality to home page

--- src/main/java/com/lankatex/smarttextile/common/controller/HomeController.java
+++ src/main/java/com/lankatex/smarttextile/common/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.lankatex.smarttextile.common.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
     // Display the LankaTex home page
     @GetMapping({"/", "/home"})
-    public String home() {
+    public String home(Model model, Principal principal) {
+
+        // Check whether a user is currently logged in
+        model.addAttribute("loggedIn", principal != null);
+
         return "home";
     }
 
